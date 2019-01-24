8cc5a8c0ba0aa4f4331b6af41d6208550cf72a00
Bryce Perez <bryceperez31@gmail.com>
2019-01-24 14:27:18 -0700

    really fun practice

--- src/App.js
+++ src/App.js
@@ -1,9 +1,71 @@
 import React, { Component } from "react";
+import axios from 'axios';
 import "./App.css";
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      customerList: []
+    };
+  }
+
+  componentDidMount(){
+    axios.get('/api/customers').then((response) => {
+        this.setState({
+          customerList: response.data
+        });
+    });
+  };
+
+  deleteCustomer(id){
+    axios.delete(`/api/customer/${id}`).then((response) => {
+        this.setState({
+          customerList: response.data
+        })
+    });
+  }
+
   render() {
-    return <div className="App">start here</div>;
+    const { customerList } = this.state;
+    const mappedCustomerList = customerList.map(customer => {
+      return (
+        <div key={customer.id} className="customer-card">
+          <span>{`${customer.id} - ${customer.name}`}</span>
+          <button onClick={() => this.deleteCustomer(customer.id)}>Delete</button>
+        </div>
+      );
+    });
+
+
+    return <div className="App">
+      <header>
+        <div>
+          <h1>Customer Page</h1>
+
+          <nav>
+            <a href="#">Home</a>
+            <a href="#">Account</a>
+            <a href="#">Support</a>
+            <a href="#">Contact</a>
+          </nav>
+
+
+        </div>
+      </header>
+
+      <section>
+        <div>
+
+            <h1>current customers</h1>
+            <div className="customer-card-container">
+            {mappedCustomerList}
+            </div>
+
+        </div>
+      </section>
+    </div>;
   }
 }
 
